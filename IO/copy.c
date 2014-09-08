0005ef5091e2edf44fbe4b3ad9484ea26cf573f5
buchenglei <myopen@yeah.net>
2014-09-08 11:32:54 +0800

    优化后的copy

--- IO/copy.c
+++ IO/copy.c
@@ -1,15 +1,15 @@
-#include <unistd.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
 int main()
 {
-	char c;
+	char c[1024];
 	int in,out;
+	int nread;
+
 	in=open("file.in",O_RDONLY);
 	out=open("file.out",O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR);
-	while(read(in,&c,1)==1){
-		write(out,&c,1);
+	while(nread=read(in,c,sizeof(c))>0){
+		write(out,c,nread);
 	}
 	exit(0);
 }
@@ -18,4 +18,10 @@ int main()
  * real    0m38.885s
  * user    0m2.044s
  * sys     0m36.758s
+ * 
+ * 优化后的结果
+ * real    0m0.111s
+ * user    0m0.004s
+ * sys     0m0.104s
+ *
  */
